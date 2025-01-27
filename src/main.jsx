5ad25f21c3c1c16fb6e9e344134b108823d56bd5
danielcalebe <danielcalebe@gmail.com>
2025-01-27 13:09:16 -0300

    Rotas

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,13 @@
-  import { StrictMode } from 'react'
-  import { createRoot } from 'react-dom/client'
-  import './index.css'
-  import App from './App.jsx'
-  import { BrowserRouter } from 'react-router-dom'
-  <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
-  createRoot(document.getElementById('root')).render(
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import { BrowserRouter } from 'react-router-dom'
 
-    <StrictMode>
-          <BrowserRouter>
-
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <BrowserRouter>
       <App />
-      </BrowserRouter>
-
-    </StrictMode>,
-  )
-
-
+    </BrowserRouter>
+  </StrictMode>
+)
