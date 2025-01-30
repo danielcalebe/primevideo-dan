3d5b0963126d0b1caa30757e8943849643d4b492
danielcalebe <danielcalebe@gmail.com>
2025-01-30 14:38:28 -0300

    Configuração de rotas do vercel 3

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,6 @@
 import { useLocation } from 'react-router-dom';
 import { assets } from '../assets/assets'
-import { useNavbarJs } from './Utils/Navbar';
-
+import { useNavbarJs } from '../Utils/navbar';
 const Navbar = () => {
         const { pathname: path } = useLocation();
     
